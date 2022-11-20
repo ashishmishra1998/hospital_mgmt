1d67aa27d89ffd1e4a5cdbbd2cc0324b702cc785
vaibhavishukla0271 <vaibhaviShukhla0271@gmail.com>
2022-11-20 11:53:19 +0530

    Cors issue

--- hospital_mgmt/settings.py
+++ hospital_mgmt/settings.py
@@ -122,8 +122,9 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:8080",
-    "http://127.0.0.1:8000",
+    # "http://localhost:8080",
+    # "http://127.0.0.1:8000", 
+    '*'
 ]
 
 REST_FRAMEWORK = {
