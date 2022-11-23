59c3e1f2f5ba506c53b6258c9876cf194364c3c8
Ubuntu <ubuntu@ip-172-31-44-241.ap-south-1.compute.internal>
2022-11-23 12:47:58 +0000

    chnages

--- hospital_mgmt/settings.py
+++ hospital_mgmt/settings.py
@@ -121,11 +121,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    # "http://localhost:8080",
-    # "http://127.0.0.1:8000", 
-    '*'
-]
+CORS_ALLOW_ALL_ORIGINS = True 
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
