a0ddb6cb9c6ac5c12453980bb73dc7c923195565
vaibhavishukla0271 <vaibhaviShukhla0271@gmail.com>
2022-11-29 15:13:16 +0530

    Fix

--- hospital_mgmt/asgi.py
+++ hospital_mgmt/asgi.py
@@ -9,26 +9,20 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 
 import os
 import hospital.routing
-import django
 
 
-# from django.core.asgi import get_asgi_application
-from channels.routing import get_default_application
+from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import hospital.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hospital_mgmt.settings')
 
-django.setup()
-
 application = ProtocolTypeRouter({
-    'http':get_default_application(),
+    'http':get_asgi_application(),
     'websocket':AuthMiddlewareStack(
         URLRouter(
             hospital.routing.websocket_urlpatterns
         )
     )
 })
-
-application = get_default_application()
