d94b45263c964da22a98884530b8aa25b4f8225c
vaibhavishukla0271 <vaibhaviShukhla0271@gmail.com>
2022-11-29 22:48:09 +0530

    Admin update

--- hospital/admin.py
+++ hospital/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import *
+admin.site.register(HospitalData)
+admin.site.register(FloorData)
+admin.site.register(WardData)
+admin.site.register(BedData)
+admin.site.register(Notification)
+
 # Register your models here.
