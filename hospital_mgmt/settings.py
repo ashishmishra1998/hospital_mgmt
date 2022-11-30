2e4890d2cf7e0bec3eb289a30fb9989ea3b3feae
vaibhavishukla0271 <vaibhaviShukhla0271@gmail.com>
2022-11-30 19:31:58 +0530

    Settings

--- hospital_mgmt/settings.py
+++ hospital_mgmt/settings.py
@@ -47,10 +47,9 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    "corsheaders.middleware.CorsMiddleware",
+    'corsheaders.middleware.CorsMiddleware',
     "django.middleware.common.CommonMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -135,13 +134,6 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 CORS_ALLOW_ALL_ORIGINS = True 
-
-CORS_ALLOWED_ORIGINS = [
-    # "http://localhost:3000",
-    # "http://127.0.0.1:8000", 
-    # '*'
-]
-CORS_ALLOW_ALL_ORIGINS : True   
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
